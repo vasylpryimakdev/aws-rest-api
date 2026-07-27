c36c71d14e8b69c29fafef4a8f3bfeadb5b526c3
Vasyl Pryimak <vasyl.pryimak.dev@gmail.com>
2026-07-27 15:03:57 +0300

    feat: implement createTodo Lambda with DynamoDB v3

--- handler/createTodo.js
+++ handler/createTodo.js
@@ -1,18 +1,54 @@
 const { DynamoDBClient } = require("@aws-sdk/client-dynamodb");
-
-const {
-  DynamoDBDocumentClient,
-  GetCommand,
-  PutCommand,
-} = require("@aws-sdk/lib-dynamodb");
+const { DynamoDBDocumentClient, PutCommand } = require("@aws-sdk/lib-dynamodb");
+const { v1: uuid } = require("uuid");
 
 const TODO_TABLE = process.env.TODO_TABLE;
-const client = new DynamoDBClient();
+
+const client = new DynamoDBClient({});
 const docClient = DynamoDBDocumentClient.from(client);
 
-exports.createTodo = async (e, ctx) => {
-  return {
-    statusCode: 200,
-    body: JSON.stringify({ message: "Hello" }),
+module.exports.createTodo = async (event) => {
+  const timestamp = new Date().getTime();
+
+  const data = JSON.parse(event.body);
+
+  if (typeof data.todo !== "string") {
+    return {
+      statusCode: 400,
+      body: JSON.stringify({
+        message: "Validation Failed",
+      }),
+    };
+  }
+
+  const item = {
+    id: uuid(),
+    todo: data.todo,
+    checked: false,
+    createdAt: timestamp,
+    updatedAt: timestamp,
   };
+
+  const params = {
+    TableName: TODO_TABLE,
+    Item: item,
+  };
+
+  try {
+    await docClient.send(new PutCommand(params));
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify(item),
+    };
+  } catch (error) {
+    console.error(error);
+
+    return {
+      statusCode: 500,
+      body: JSON.stringify({
+        message: "Could not create todo",
+      }),
+    };
+  }
 };
